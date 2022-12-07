10fa15a50696053a2b5042928814a0fb02a5ca11
bababab <bruno.cleymand@gmail.com>
2022-12-07 13:00:13 +0100

    Co-authored-by: Rustyymp4 <Rustyymp4@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from ".//pages/home.js";
+import PkmnList from ".//pages/pkmnlist.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//App.js
+function App(props){
+  return <Router>
+      <Switch>
+        <Route exact path="/"> {/*ici on met l'URL dans le navigateur*/}
+          <Home /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route path="/pkmnlist"> {/*ici on met l'URL dans le navigateur*/}
+          <PkmnList /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+      </Switch>
+  </Router>
 }
 
 export default App;
---
+++ src/pages/pkmnlist.js
@@ -0,0 +1,5 @@
+function PkmnList(props){
+    return <h1>ALLLLL the pokemomns</h1>;
+}
+
+export default PkmnList;
